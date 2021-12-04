f1b5cebefd99932365e95eabeb13e6813942e5eb
raoufgr <raouf.grera@gmail.com>
2021-12-04 21:20:04 +0200

    Delete android app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@ $notes = $company->getNote();
                     <p style="text-align: center;font-size: 17px; color: #373737;"><span itemprop="description">نشر وبحث عن وظائف شاغرة في ليبيا، وتصميم السيرة الذاتية باللغة العربية.</span></p>
 
                     <div style="display: block;text-align: center; margin-bottom: 8px">
-                    <a  href='https://play.google.com/store/apps/details?id=libyacvpro.libya_cv'><img style="margin: 9px;" width="194px" alt='Get it on Google Play' src='{{ asset('images/simple/en_badge.png') }}'/></a>
+               <!--     <a  href='https://play.google.com/store/apps/details?id=libyacvpro.libya_cv'><img style="margin: 9px;" width="194px" alt='Get it on Google Play' src='{{ asset('images/simple/en_badge.png') }}'/></a> -->
                     <a href="https://facebook.com/LibyaCV" title="Facebook" style="    padding: 16px 16px;" class="btn btn-facebook btn-lg"><i class="icon-facebook-official"></i> تابعنا علي فيسبوك</a>
                     <a href="/donate" title="Facebook" style="/* padding: 16px 16px; */margin: 4px;" class="btn btn-default btn-lg"><span style="
                         font-size: 18px;font-weight: 500;">Donate cryptocurrency</span><br><img style="/* margin: 9px; */width: 60px;" width="194px" alt="Donate cryptocurrency" src="{{ asset('images/simple/clog.png') }}"> </a>
